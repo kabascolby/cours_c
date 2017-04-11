4164821c0e6f4b07a0ebf371f34bca8518f273ea
kabascolby <kabascolby@gmail.com>
2017-04-11 14:12:06 -0700

    creation de rush
    cretion du dossier rush

--- rush04.c
+++ rush04.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush02.c                                           :+:      :+:    :+:   */
+/*   rush04.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkaba <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 21:45:01 by lkaba             #+#    #+#             */
-/*   Updated: 2017/04/09 18:14:33 by nwang            ###   ########.fr       */
+/*   Updated: 2017/04/09 18:39:05 by nwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,20 @@ void	line2(int ix, int x)
 
 void	line3(int ix, int x)
 {
-	if (ix == 1 || ix == x)
+	if (ix == 1)
 		ft_putchar('C');
+	else if (ix == x)
+		ft_putchar('A');
 	else
 		ft_putchar('B');
 }
 
 void	line1(int ix, int x)
 {
-	if (ix == 1 || ix == x)
+	if (ix == 1)
 		ft_putchar('A');
+	else if (ix == x)
+		ft_putchar('C');
 	else
 		ft_putchar('B');
 }
