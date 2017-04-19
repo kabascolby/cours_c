9b4ca2d6a4ea9706079362d3ec4ab11461cbd93f
Lamine Kaba <lkaba@student.42.us.org>
2017-04-18 17:02:11 -0700

    les ponteurs sur fonctions ex0s

--- function_pointers.c
+++ function_pointers.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
 
+typedef void (*funptr)(char);
+
 void ft_putchar(char c)
 {
     write(1, &c, 1);
@@ -7,7 +9,8 @@ void ft_putchar(char c)
 
 int     main()
 {
-    void (*f)(char);
+    funptr      f;
+   // void (*f)(char);
     f = &ft_putchar;
     f('z');
     return (0);
