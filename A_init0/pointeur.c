4815c57279f63372ba9a031b6afe4c60fc1e4b9b
Lamine Kaba <lkaba@student.42.us.org>
2017-12-07 19:22:09 -0800

    update print_reverse

--- A_init0/pointeur.c
+++ A_init0/pointeur.c
@@ -66,9 +66,42 @@ void ft_putnbr(int n)
 	}
 }
 
+void ft_ultimate_ft(int *********nbr)
+{
+   	*********nbr = 42;
+}
+
 int main(void)
 {
-	int tab[10];
+	int 	n;
+	int 	*ptr;
+	int 	***ptr2;
+	int 	***ptr3;
+	int 	****ptr4;
+	int 	*****ptr5;
+	int 	******ptr6;
+	int 	*******ptr7;
+	int 	********ptr8;
+	int 	*********ptr9;
+	
+	ptr = &n;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	n = 56;
+
+	ft_ultimate_ft(ptr);
+
+
+
+	ft_putnbr(n);
+
+	/*int tab[10];
 	int tab2[10];
 	int *ptr[2];
 	ptr[0] = tab;
@@ -77,6 +110,7 @@ int main(void)
 	*(*ptr + 3) = 8;
 	ft_putnbr(tab[3]);
 	ft_putchar('\n');
-//	ft_putnbr(tab[3]);
+//	ft_putnbr(tab[3]);*/
+	A
 	return (0);
 }
--- autre projets/README.md
+++
@@ -1,3 +0,0 @@
-# 42-Piscine-C
-
-Mes rendus lors de la piscine de C de 42. A vos risques et périls
