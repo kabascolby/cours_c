9730cd42429a5d256ceb8bf386edf88a771374cc
Lamine Kaba <lkaba@student.42.us.org>
2018-04-04 19:25:47 -0700

    version 3 of my pritf

--- 42/main.c
+++ 42/main.c
@@ -6,7 +6,7 @@
 /*   By: lkaba <lkaba@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 20:15:49 by lkaba             #+#    #+#             */
-/*   Updated: 2018/04/03 19:42:09 by lkaba            ###   ########.fr       */
+/*   Updated: 2018/04/04 18:25:20 by lkaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,11 @@
 #include <stdio.h>
 #include <limits.h>
 
-void ft_struct_check(t_args *flags)
-{
-    printf("plus: %d\n", flags->plus);
-    printf("min: %d\n", flags->min);
-    printf("space: %d\n", flags->space);
-    printf("hash: %d\n", flags->hash);
-    printf("zero: %d\n", flags->zero);
-    printf("prec: %d\n", flags->prec);
-    printf("precis: %d\n", flags->precis);
-    printf("field_w: %d\n", flags->field_w);
-    printf("length: %d\n", flags->length);
-    printf("type: %c\n", flags->type);
-}
-
 int main()
 {
     unsigned long l;
     //unsigned short sh = USHRT_MAX;
-    printf("\n%d", ft_printf("%#p", 0));
+    printf("\n%d", ft_printf("%q", 345));
   // printf("\n%d", ft_printf("%3c", 0));
     return 0;
 }
