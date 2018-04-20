3928c8c4c71f65d6e164c6a669a751a552be2911
Lamine Kaba <lkaba@student.42.us.org>
2018-04-19 20:03:40 -0700

    girdle

---
+++ 42/hercules/girdle/gridle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import getpass
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+fromaddr = "ladainianwade@gmail.com"
+passwd = getpass.getpass('your email server password : ')
+toaddr = raw_input("ADDRESS YOU WANT TO SEND TO:")
+subj = raw_input("SUBJECT OF MAIL:")
+message = raw_input("MESSAGE OF EMAIL:")
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = subj
+
+body = message
+msg.attach(MIMEText(body, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, passwd)
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+print "Email sent!"
+server.quit()
