2ae55567e6a751b2f406280169f53aded7d50437
kabascolby <kabascolby@gmail.com>
2019-06-23 11:00:51 -0700

    adding .vscode to git ignore

---
+++ webdev/javascript/class/example2/index.js
@@ -0,0 +1,38 @@
+/* const circle = {
+    radius : 1,
+    location: {
+        x : 1,
+        y : 1,
+    },
+    draw: function(){
+        console.log('draw');
+    }
+}; */
+
+// factory function
+
+function createCircle(radius){
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    }
+};
+
+const circle = createCircle(1);
+
+circle.draw();
+console.log(circle.radius);
+
+// constructor function
+
+function CreateCircle2(radius){
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+};
+
+const circle2 = new CreateCircle2(1);
+circle2.draw();
